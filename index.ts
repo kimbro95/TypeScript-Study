daedd16a2aa92dbc746d4863d83b44cf90798abe
l995lol4 <gudgudgud714@naver.com>
2022-02-22 22:51:16 +0900

    #8 함수와 methods에 type alias 지정하는 법

--- index.ts
+++ index.ts
@@ -1,61 +1,85 @@
 /*
-    Literal Types
+    함수와 methods에 type alias를 지정하는 법
 
-    - Literal Type 만드는 법
-    ex) let kim : '개발자';
-        let lee : '기획자';
-    string, number 이런 것이 아닌 일반 글자도 타입이 될 수 있다.
-    ⭐특정 글자나 숫자만 가질 수 있게 제한을 두는 타입⭐
-    
-    ex) let 방향 : 'left' | 'right';
-        방향 = 'left';
-    OR 기호를 사용하여 union type 으로도 지정할 수 있다.
+    - function type
+    ex) type NumOut = ( x : number, y : number ) => number;
+            타입명 = (파라미터 타입) => 리턴타입;
 
-    ex) function 함수 ( a : 'hello' ) : 1 | 0 {
-        return 1
+        let ABC : NumOut = ( x, y ) => {
+            return x + y
+        }
+    함수에 타입을 지정할 때는 위와 같이 선언한다.
+
+    - methods 안에 타입지정하기
+    ex) let 회원정보 = {
+        name : 'kim',
+        age : 28,
+        plusOne ( x ){
+            return x + 1
+        },
+        changeName : () => {
+            console.log('hello')
+        }
     }
-    함수도 마찬가지로 파라미터 또는 return 타입을 위와 같이 선언할 수 있다.
+    회원정보.plusOne(3);
+    회원정보.changeName();
 
-    Q1. 함수를 만들어보세요.
-        1. '가위', '바위', '보' 문자들만 파라미터로 입력할 수 있고
-        2. '가위', '바위', '보' 라는 문자들만 담을 수 있는 array 자료만 return 할 수 있습니다.
-        3. 예를 들면 ['가위', '보', '가위'] 이런거 return 가능
-        4. ['가위', '바보'] 이런거 return하면 에러나야함 
-    
-    A. function 함수 ( x : '가위' | '바위' | '보' ) : ( '가위' | '바위' | '보' )[] {
-        return [ '가위', '바위' ]
-    }
+    object 함수안에 arrow function, 일반함수 전부 집어넣을 수 있다.
 
-    const의 업그레이드버전
-    ex) const 이름 = 'kim' | 'lee' 
-        ( 자바스크립트에서는 이런 문법이 불가능 하지만 타입스크립트는 literal type으로 가능하다. )
+    Q1. 위에 method 코드 예시에서 회원정보라는 변수에 타입을 지정해보자.
+        1. plusOne이라는 속성은 함수여야하고, 숫자를 넣어서 숫자를 뱉는 함수여야합니다.
+        2. changeName이라는 속성은 함수여야하고, 아무것도 return하면 안됩니다. 
+        3. type 키워드를 쓰든 말든 알아서 합시다. 
 
-    - as const
-    ex) var 자료 = {
-            name : 'kim'
+    A.  type 회원정보 = {
+            name : string,
+            age : number,
+            plusOne : ( x : number ) => number,
+            changeName : () => void
         }
-        
-        function 함수 ( x : 'kim' ){
 
+    Q2. 다음 함수 2개를 만들고 타입을 정의하기
+        1. cutZero()라는 함수를 만듭시다. 이 함수는 문자를 하나 입력하면 맨 앞에 '0' 문자가 있으면 제거하고 문자 type으로 return 해줍니다.
+        2. removeDash()라는 함수를 만듭시다. 이 함수는 문자를 하나 입력하면 대시기호 '-' 가 있으면 전부 제거해주고 그걸 숫자 type으로 return 해줍니다. 
+        3. 함수에 타입지정시 type alias를 꼭 써보도록 합시다. 
+
+    A.  type Cut = ( x : string ) => string;
+        let cutZero : Cut = ( x ) => {
+            let result = x.replace(/^0+/, "");
+            return result;
         }
-        함수(자료.name) // <= error
 
-    위의 예시처럼 작성시 함수(자료.name) 라인에서 에러가 뜬다.
-    함수의 'kim' 은 타입이지만 자료.name 은 string 타입의 object 속성의 값 'kim' 이기 때문이다.
-
-    위의 코드 해결법
-    1. object 작성시 타입을 정하기
-    2. assertion 사용하기 ( ex.as 'kim' )
-    3. as const 사용하기
-    ex) as const
-        var 자료 = {
-            name : 'kim'
-        } as const;
-        function 함수 ( x : 'kim' ){
+        let removeDash = ( x : string ) : number => {
+            let result = x.replace(/-/g, "");
+            return parseInt(result);
         }
-        함수(자료.name)
 
-    as const 특징
-    1. 타입을 object 의 value 값으로 바꿔준다. ( 타입이 'kim' 으로 변경 )
-    2. object 안에 모든 속성을 readonly 속성으로 변경된다.
+    Q3. 함수에 함수를 집어 넣기
+        Q2 에서 만든 함수들을 파라미터로 넣을 수 있는 함수를 제작하고 싶은 것입니다. 
+        이 함수는 파라미터 3개가 들어가는데 첫째는 문자, 둘째는 함수, 셋째는 함수를 집어넣을 수 있습니다. 이 함수를 실행하면
+        1. 첫째 파라미터를 둘째 파라미터 (함수)에 파라미터로 집어넣어줍니다.
+        2. 둘째 파라미터 (함수)에서 return된 결과를 셋째 파라미터(함수)에 집어넣어줍니다.
+        3. 셋째 파라미터 (함수)에서 return된 결과를 콘솔창에 출력해줍니다. 
+        이 함수는 어떻게 만들면 될까요?
+        둘째 파라미터엔 cutZero, 셋째 파라미터엔 removeDash 라는 함수들만 입력할 수 있게 파라미터의 타입도 지정해봅시다.
+
+    A.  type Cut = ( x : string ) => string;
+        let cutZero : Cut = ( x ) => {
+            let result = x.replace(/^0+/, "");
+            return result;
+        }
+
+        let removeDash = ( x : string ) : number => {
+            let result = x.replace(/-/g, "");
+            return parseFloat(result);
+        }
+
+        type Ytype = ( a : string ) => string;
+        type Ztype = ( a : string ) => number;
+        function 만든함수( x : string, y : Ytype, z : Ztype){
+            let result = y(x);
+            let result2 = z(result);
+            console.log(result2);
+        }
+        만든함수('010-1111-2222', cutZero, removeDash);
 */
