ee7599051a5a97d999fcbf49beec5828744e6cc0
l995lol4 <gudgudgud714@naver.com>
2022-02-23 14:00:20 +0900

    #9 타입스크립트로 HTML 변경과 조작할 때 주의점

--- index.ts
+++ index.ts
@@ -1,85 +1,93 @@
 /*
-    함수와 methods에 type alias를 지정하는 법
+    타입스크립트로 HTML 변경과 조작할 때 주의점
 
-    - function type
-    ex) type NumOut = ( x : number, y : number ) => number;
-            타입명 = (파라미터 타입) => 리턴타입;
-
-        let ABC : NumOut = ( x, y ) => {
-            return x + y
+    - strictNullCheck 옵션
+    ex) {
+            "compilerOptions" : {
+                "target" : "ES5",
+                "module" : "commonjs",
+                "strictNullCheck" : true  
+            }
         }
-    함수에 타입을 지정할 때는 위와 같이 선언한다.
+    tsconfig.json 파일에 strictNullCheck 옵션을 true 값으로 줄 경우
+    변수를 조작하기 이전에 null 값인지 아닌지 체크할 수 있다.
 
-    - methods 안에 타입지정하기
-    ex) let 회원정보 = {
-        name : 'kim',
-        age : 28,
-        plusOne ( x ){
-            return x + 1
-        },
-        changeName : () => {
-            console.log('hello')
-        }
+    - HTML 찾고 변경하기
+    ex) let title = document.querySelector("#title");
+        title.innerHTML = "HI"
+
+        strict 옵션이 true 로 설정했기때문에 에러가 출력된다
+        에러의 원인은 셀렉터로 찾은 html 의 타입은 Element | null 두가지 타입의 union type 이기 때문이다.
+        ( 찾지 못 했을 경우에는 null )
+
+
+    - 해결을 위한 narrowing 방법
+    1. 기본적인 narrowing
+    let title = document.querySelector("#title");
+    if ( title !== null ){
+        title.innerHTML = 'Hi'
     }
-    회원정보.plusOne(3);
-    회원정보.changeName();
 
-    object 함수안에 arrow function, 일반함수 전부 집어넣을 수 있다.
+    2. instanceof 를 이용한 narrowing
+    let title = document.querySelector("#title");
+    if( title instanceof Element ){
+        title.innerHTML = 'Hi'
+    }
 
-    Q1. 위에 method 코드 예시에서 회원정보라는 변수에 타입을 지정해보자.
-        1. plusOne이라는 속성은 함수여야하고, 숫자를 넣어서 숫자를 뱉는 함수여야합니다.
-        2. changeName이라는 속성은 함수여야하고, 아무것도 return하면 안됩니다. 
-        3. type 키워드를 쓰든 말든 알아서 합시다. 
+    3. assertion 을 이용한 narrowing
+    let title = document.querySelector("#title") as Element;
+    title.innerHTML = 'Hi'
 
-    A.  type 회원정보 = {
-            name : string,
-            age : number,
-            plusOne : ( x : number ) => number,
-            changeName : () => void
+    4. optional chaining 연산자를 이용한 narrowing
+    let title = document.querySelector("#title");
+    if( title?.innerHTML !== undefined ){
+        title.innerHTML = 'Hi'
+    }
+
+    5. strictNullCheck 옵션을 false 로 설정하기
+
+    가장 좋은 해결책은 2번의 instanceof 를 이용한 narrowing 이다.
+    3번 또는 5번은 해결책이긴 하지만 그렇다면 타입스크립트를 쓰는 의미가 없다.
+
+
+    - a태그의 href 속성 바꾸기
+    ex) let link = document.querySelector(".link");
+        if( link instanceof HTMLAnchorElement ){
+            link.href = 'daum.net'
+        }
+    html 태그에는 종류별로 정확한 타입명칭이 있다
+        a 태그   - HTMLAnchorElement
+        img 태그 - HTMLImageElement
+        h4 태그  - HTMLHeadingElement
+        button 태그 - HTMLButtonElement
+    이렇게 태그별로 정확한 타입으로 narrowing을 해야한다.
+
+
+    - 이벤트리스너에서 narrowing
+    let button = document.getElementById('#button');
+    button?.addEventListener('click', function(){
+        console.log('hi');
+    });
+    optional chaining 문법을 이용한 narrowing
+
+    Q1. 이미지를 바꿔보세요.
+    이미지를 new.jpg 로 변경해보세요
+    ex) <img id="image" src="img.jpg" />
+
+    A.  let imgSrc = document.querySelector("#image");
+        if( imgSrc instanceof HTMLImageElement){
+            imgSrc.src = 'new.jpg';
         }
 
-    Q2. 다음 함수 2개를 만들고 타입을 정의하기
-        1. cutZero()라는 함수를 만듭시다. 이 함수는 문자를 하나 입력하면 맨 앞에 '0' 문자가 있으면 제거하고 문자 type으로 return 해줍니다.
-        2. removeDash()라는 함수를 만듭시다. 이 함수는 문자를 하나 입력하면 대시기호 '-' 가 있으면 전부 제거해주고 그걸 숫자 type으로 return 해줍니다. 
-        3. 함수에 타입지정시 type alias를 꼭 써보도록 합시다. 
+    Q2. 여러개의 html요소 바꾸기
+    ex) <a class="naver" href="naver.com">링크</a>
+        <a class="naver" href="naver.com">링크</a>
+        <a class="naver" href="naver.com">링크</a> 
 
-    A.  type Cut = ( x : string ) => string;
-        let cutZero : Cut = ( x ) => {
-            let result = x.replace(/^0+/, "");
-            return result;
-        }
-
-        let removeDash = ( x : string ) : number => {
-            let result = x.replace(/-/g, "");
-            return parseInt(result);
-        }
-
-    Q3. 함수에 함수를 집어 넣기
-        Q2 에서 만든 함수들을 파라미터로 넣을 수 있는 함수를 제작하고 싶은 것입니다. 
-        이 함수는 파라미터 3개가 들어가는데 첫째는 문자, 둘째는 함수, 셋째는 함수를 집어넣을 수 있습니다. 이 함수를 실행하면
-        1. 첫째 파라미터를 둘째 파라미터 (함수)에 파라미터로 집어넣어줍니다.
-        2. 둘째 파라미터 (함수)에서 return된 결과를 셋째 파라미터(함수)에 집어넣어줍니다.
-        3. 셋째 파라미터 (함수)에서 return된 결과를 콘솔창에 출력해줍니다. 
-        이 함수는 어떻게 만들면 될까요?
-        둘째 파라미터엔 cutZero, 셋째 파라미터엔 removeDash 라는 함수들만 입력할 수 있게 파라미터의 타입도 지정해봅시다.
-
-    A.  type Cut = ( x : string ) => string;
-        let cutZero : Cut = ( x ) => {
-            let result = x.replace(/^0+/, "");
-            return result;
-        }
-
-        let removeDash = ( x : string ) : number => {
-            let result = x.replace(/-/g, "");
-            return parseFloat(result);
-        }
-
-        type Ytype = ( a : string ) => string;
-        type Ztype = ( a : string ) => number;
-        function 만든함수( x : string, y : Ytype, z : Ztype){
-            let result = y(x);
-            let result2 = z(result);
-            console.log(result2);
-        }
-        만든함수('010-1111-2222', cutZero, removeDash);
+    A.  let link = document.querySelectorAll('.naver');
+        link.forEach((val) => {
+            if( val instanceof HTMLAnchorElement ){
+                val.href = 'daum.net'
+            }
+        })
 */
